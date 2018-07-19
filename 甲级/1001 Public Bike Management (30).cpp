765e2939cb90d0d59b72aab72d024f738d20acab
Gingko-C <cbg0202@gmail.com>
2018-07-19 13:03:48 +0800

    Public Bike Management
    Dijkstra

--- 甲级/1001 Public Bike Management (30).cpp
+++ 甲级/1001 Public Bike Management (30).cpp
@@ -1,100 +1,94 @@
-//错误：未完成！
-#include <string>
-using std::string;
+//Dijkstra 改进加入 路径记录和等权重路径记录
 #include <vector>
 using std::vector;
-#include <map>
-using std::map;
-#include <set>
-using std::set;
-#include <list>
-using std::list;
 #include <iostream>
-using std::cout;
 using std::cin;
+using std::cout;
 using std::endl;
-#include <memory>
-using std::shared_ptr;
-using std::unique_ptr;
-using std::make_shared;
-using std::allocator;
-#include <new>
 #include <algorithm>
-#include <utility>
-#include <sstream>
-
+ 
+#define INT_MAX 100000
 int main() {
-	string line;
-	int cmax, stanum, index, roadnum;
-	map<std::pair<int, int>, int> timecost;
-	cin >> cmax >> stanum >> index >> roadnum;
-	vector<vector<int>> fastroad(stanum + 1, { 0 });
-	vector<std::pair<int, int>> ctrans(stanum + 1, { 0,0 });
-	vector<int> csta(1, -1);
-	for (int i = 0;i < stanum;i++) {
-		int temp;
-		cin >> temp;
-		csta.push_back(temp);
-	}
-	for (int i = 0;i < roadnum;i++) {
-		std::pair<int, int> temp_pair;
-		int time;
-		cin >> temp_pair.first >> temp_pair.second >> time;
-		timecost[temp_pair] = time;
-		timecost[{temp_pair.second, temp_pair.first}] = time;
-		if (temp_pair.first == 0) {
-			fastroad[temp_pair.second] = { 0, temp_pair.second };
-			int temp_ctrans = csta[temp_pair.second] - cmax / 2;
-			if (temp_ctrans > 0)
-				ctrans[temp_pair.second].second = temp_ctrans;
-			else ctrans[temp_pair.second].first = -temp_ctrans;
-		}
-	}
-	for (int i = 1;i <= index;i++) {
-		for (int j = 0;j <= stanum;j++) {
-			if (timecost[{0, j}] != 0 && timecost[{j, i}] != 0) {
-				int temp = timecost[{0, j}] + timecost[{j, i}];
-				int temp_ctrans = csta[i] - cmax / 2 + ctrans[j].second - ctrans[j].first;
-				if (timecost[{0, i}] == 0) {
-					timecost[{0, i}] = temp;
-					if (temp_ctrans > 0) ctrans[i].second = temp_ctrans;
-					else ctrans[i].first = -temp_ctrans;
-					fastroad[i] = fastroad[j];
-					fastroad[i].push_back(i);
-				}
-				else {
-					if (temp == timecost[{0, i}]) {
-						if (abs(temp_ctrans) < abs(ctrans[i].second - ctrans[i].first)) {
-							if (temp_ctrans > 0)
-								ctrans[i] = { 0,temp_ctrans };
-							else ctrans[i] = { -temp_ctrans,0 };
-							timecost[{0, i}] = temp;
-							fastroad[i] = fastroad[j];
-							fastroad[i].push_back(i);
-						}
-					}
-					else {
-						if (temp < timecost[{0, i}]) {
-							timecost[{0, i}] = temp;
-							if (temp_ctrans > 0)
-								ctrans[i] = { 0,temp_ctrans };
-							else ctrans[i] = { -temp_ctrans,0 };
-							fastroad[i] = fastroad[j];
-							fastroad[i].push_back(i);
-						}
-					}
-				}
-			}
-		}
-	}
-	cout << ctrans[index].first << " ";
-	int flag = 0;
-	for (auto i : fastroad[index]) {
-		if (flag != 0) cout << "->";
-		if (flag == 0) flag = 1;
-		cout << i;
-	}
-	cout << " " << ctrans[index].second << endl;
-	system("pause");
-	return 0;
+    int cmax, vnum, pnum, wnum;
+    cin >> cmax >> vnum >> pnum >> wnum;
+    int bv, ev;
+    int baseP = 0;
+    vector<int> cnum(++vnum); //实际结点数要加上0结点
+    vector<vector<int>> wmat(vnum, vector<int>(vnum, INT_MAX));
+    vector<int> dis(vnum, INT_MAX);
+    vector<bool> flag(vnum, false);
+    vector<vector<vector<int>>> sw(vnum);
+    vector<vector<int>> w2p;
+    for (int i = 1;i < vnum;i++) {
+        cin >> cnum[i];
+    }
+    for (int i = 0;i < wnum;i++) {
+        cin >> bv >> ev;
+        cin >> wmat[bv][ev];
+        wmat[ev][bv] = wmat[bv][ev];
+        if (bv == 0) {
+            dis[ev] = wmat[bv][ev];
+            sw[ev] = { {0,ev} };
+        }
+        if (ev == 0) {
+            dis[bv] = wmat[ev][bv];
+            sw[bv] = { { 0,bv } };
+        }
+ 
+    }
+    dis[0] = 0;
+    flag[0] = true;
+    for (int i = 0;i < vnum - 1;i++) {
+        int min = INT_MAX;
+        for (int j = 1;j < vnum;j++) {
+            if (flag[j] != true && dis[j] < min) {
+                min = dis[j];
+                baseP = j;
+            }
+        }
+        flag[baseP] = true;
+        for (int j = 1;j < vnum;j++) {
+            int temp_dis = (wmat[baseP][j] == INT_MAX) ? INT_MAX : dis[baseP] + wmat[baseP][j];
+            if (temp_dis < dis[j]) {
+                dis[j] = temp_dis;
+                sw[j] = sw[baseP];
+                for (auto &w : sw[j]) {
+                    w.push_back(j);
+                }
+            }
+            else if (temp_dis == dis[j]) {
+                for (auto w : sw[baseP]) {
+                    vector<int> temp_v(w);
+                    temp_v.push_back(j);
+                    sw[j].push_back(temp_v);
+                }
+            }
+        }
+    }
+    for (int i = 0;i < sw[pnum].size();i++) {
+        int sent = 0, carry = 0;
+        for (int j = 1;j < sw[pnum][i].size();j++) {
+            carry += cnum[sw[pnum][i][j]] - cmax / 2;
+            if (carry < 0) {
+                sent += -carry;
+                carry = 0;
+            }
+        }
+        w2p.push_back({ sent,carry,i });
+    }
+ 
+    std::sort(w2p.begin(), w2p.end());
+ 
+    cout << w2p[0][0]<<" ";
+    int flag_cout = false;
+    for (auto i : sw[pnum][w2p[0][2]]) {
+        if (flag_cout == false) {
+            cout << i;
+            flag_cout = true;
+        }
+        else cout << "->" << i;
+    }
+    cout << " " << w2p[0][1] << endl;
+    system("pause");
+    return 0;
 }
